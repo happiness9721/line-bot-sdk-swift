b7524c33866abe1355c48c48c90794f1660d4264
Joe Jiang <happiness9721@gmail.com>
2018-09-20 15:27:07 +0800

    fix: Correct LineErrorTests's allTests format.

--- Tests/LineBotTests/LineResponseTests/LineErrorTests.swift
+++ Tests/LineBotTests/LineResponseTests/LineErrorTests.swift
@@ -54,7 +54,7 @@ class LineErrorTests : XCTestCase {
   }
 
   static let allTests = [
-    ("testDecodeErrorResponse", testDecodeErrorResponse,
-     "testInvalidReplyTokenErrorResponse", testInvalidReplyTokenErrorResponse)
+    ("testDecodeErrorResponse", testDecodeErrorResponse),
+    ("testInvalidReplyTokenErrorResponse", testInvalidReplyTokenErrorResponse)
   ]
 }
